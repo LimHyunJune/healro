55bf1ec342303d73d8c95b4c0b2437b9d1e67636
임현준 <baj2121@naver.com>
2023-08-06 18:28:37 +0900

    gitignore

---
+++ healro/src/main/java/com/imukun/healro/HealroApplication.java
@@ -0,0 +1,13 @@
+package com.imukun.healro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealroApplication.class, args);
+	}
+
+}
---
+++ healro/src/test/java/com/imukun/healro/HealroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imukun.healro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
