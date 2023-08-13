224446927571744ef143a4858f002590b7912df2
임현준 <baj2121@naver.com>
2023-08-13 20:04:11 +0900

    img path modify

---
+++ healro/src/main/java/com/imukun/healro/diagnosis/DiagnosisRestController.java
@@ -0,0 +1,14 @@
+package com.imukun.healro.diagnosis;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class DiagnosisRestController {
+
+
+}
--- healro/src/main/resources/templates/js/scripts.js
+++ healro/src/main/resources/templates/js/scripts.js
@@ -1,7 +1,18 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$( document ).ready(function() {
+});
+
+
+$('#submit').on('click',function(){
+    var data = {
+
+    }
+    $.ajax({
+        type: "POST",
+        dataType: "jsonp",
+        data : data,
+        url: "http://localhost:5000/predict",
+        success: function (data) {
+
+        }
+    });
+})
